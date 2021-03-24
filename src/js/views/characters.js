b60012daf8b9f92d2275a5257644cb9ae7434ebc
bramag94 <bramag94@hotmail.com>
2021-03-24 02:31:42 +0000

    versión con estructura y diseño

--- src/js/views/characters.js
+++ src/js/views/characters.js
@@ -7,23 +7,21 @@ export const Characters = () => {
 	const { store } = useContext(Context);
 	console.log("data", store.peoples);
 	return (
-		<div className="text-center mt-5">
+		<div className="container text-center mt-5">
 			<h1>Personajes</h1>
 
-			<div className="container">
-				<ul className="list-group">
-					{store.peoples.map((item, index) => {
-						return (
-							<div key={index}>
-								<Card name={item.name} hair={item.hair_color} eyes={item.eyes_color} />;
-							</div>
-						);
-					})}
-					<Link to="/">
-						<button className="btn btn-primary">Ir a home</button>
-					</Link>
-				</ul>
+			<div className="card-deck">
+				{store.peoples.map((item, index) => {
+					return (
+						<div key={index} className="col-md-4 mb-3">
+							<Card name={item.name} hair={item.hair_color} eyes={item.eye_color} />;
+						</div>
+					);
+				})}
 			</div>
+			<Link to="/">
+				<button className="btn btn-primary">Ir a home</button>
+			</Link>
 		</div>
 	);
 };
--- src/js/views/planets.js
+++ src/js/views/planets.js
@@ -7,24 +7,23 @@ export const Planets = () => {
 	const { store } = useContext(Context);
 	console.log("data", store.planets);
 	return (
-		<div classNameName="text-center mt-5">
+		<div className=" container text-center mt-5">
 			<h1>Planets</h1>
-
-			<div className="container">
-				<ul className="list-group">
+			<div className>
+				<div className="card-deck">
 					{store.planets.map((item, index) => {
 						return (
-							<div key={index}>
+							<div key={index} className="col-md-4 mb-3">
 								<Cardplanetas name={item.name} diameter={item.diameter} climate={item.climate} />;
 							</div>
 						);
 					})}
-
-					<Link to="/">
-						<button className="btn btn-primary">Ir a home</button>
-					</Link>
-				</ul>
+				</div>
 			</div>
+
+			<Link to="/">
+				<button className="btn btn-primary">Ir a home</button>
+			</Link>
 		</div>
 	);
 };
