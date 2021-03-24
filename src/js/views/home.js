95ded9b442489b4cd7f8426eb76ec57ad7aac478
bramag94 <bramag94@hotmail.com>
2021-03-24 02:57:33 +0000

    diseño final de home

--- src/js/views/home.js
+++ src/js/views/home.js
@@ -3,13 +3,38 @@ import { Link } from "react-router-dom";
 
 export const Home = () => (
 	<div className="text-center mt-5">
-		<h1>Star Wars</h1>
-		<Link to="/planets">
-			<botton className="btn btn-primary">Ir a Planets</botton>
-		</Link>
-		<span> </span>
-		<Link to="/characters">
-			<botton className="btn btn-primary">Ir a Personajes</botton>
-		</Link>
+		<div className="d-flex flex-column">
+			<div>
+				<h1>Star Wars</h1>
+			</div>
+			<span />
+			<div className="d-flex justify-content-center">
+				<div>
+					<Link to="/planets">
+						<h1>Planetas</h1>
+						<img
+							width={300}
+							height={200}
+							src="https://icons.iconarchive.com/icons/artua/star-wars/256/Death-Star-icon.png"
+							alt="Planets"
+							border-radius="5"
+						/>
+					</Link>
+				</div>
+				<span> </span>
+				<div>
+					<Link to="/characters">
+						<h1>Personajes</h1>
+						<img
+							width={300}
+							height={200}
+							src="https://lh3.googleusercontent.com/proxy/mmCGzpt0hDAwHhqfrGHnRqBRLH_JuQtSC1ta0FxWu4cactocATMYo_PBLPTOf1BqLuL6_7fP6P1siyCJS6cPgZwm2o63VGAc0FUxvJE7AL2R9-VcdkJ8R39q890fYzc"
+							alt="Baby Yoda"
+							border-radius="5"
+						/>
+					</Link>
+				</div>
+			</div>
+		</div>
 	</div>
 );
