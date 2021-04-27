ac942c4e7c7979dfd4af4c58246376b081d0966b
bramag94 <bramag94@hotmail.com>
2021-04-27 23:40:42 +0000

    card details

--- src/js/component/planetdetails.js
+++ src/js/component/planetdetails.js
@@ -1,24 +1,32 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { Link } from "react-router-dom";
 import { useParams } from "react-router-dom";
 import { Context } from "../store/appContext";
-import PropTypes from "prop-types";
+import { PropTypes } from "prop-types";
 
-export const Planetdetails = () => {
+export const Planetdetails = props => {
 	const { store } = useContext(Context);
-	const paramid02 = useParams();
-	console.log(store.planets);
+	const { id02 } = useParams();
+	console.log(id02);
+
+	const [planets, Planetdetails] = React.useState([]);
 
 	return (
-		<div className="card" style={{ width: "18rem;" }}>
-			<img src="..." className="card-img-top" alt="..." />
-			<div className="card-body">
-				<h5 className="card-title"> {this.props.name} </h5>
-				<p className="card-text"> {this.props.diameter}</p>
-				<p className="card-text"> {this.props.climate}</p>{" "}
-				<Link to={"/planetdetails/" + props.index}>
-					<button className="btn btn-primary">Ver más</button>
-				</Link>
+		<div className="container">
+			<div className="card" style={{ width: "18rem" }}>
+				<img
+					src="https://static2.abc.es/media/201201/19/Star_Wars--644x450.jpg"
+					className="card-img-top"
+					alt="..."
+				/>
+				<div className="card-body">
+					<h5 className="card-title"> Nombre: {props.name} </h5>
+					<p className="card-text"> Diámero {props.diameter}</p>
+					<p className="card-text"> Tipo de clima: {props.climate}</p>{" "}
+					<Link to={"/planets"}>
+						<button className="btn btn-primary">Ir atrás</button>
+					</Link>
+				</div>
 			</div>
 		</div>
 	);
@@ -26,7 +34,7 @@ export const Planetdetails = () => {
 
 Planetdetails.propTypes = {
 	name: PropTypes.string,
-	diameter: PropTypes.string,
+	diameter: PropTypes.number,
 	climate: PropTypes.string,
 	index: PropTypes.number
 };
